bc0c95d25494909e1b3c2adf1bcfff1009bb8356
Shaun Thomas <shaunthomas999@gmail.com>
2017-08-17 21:28:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kotlin Inventory Sample App
+
---
+++ src/test/kotlin/com/shaunthomas999/inventory/KotlinInventorySampleAppApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.shaunthomas999.inventory
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinInventorySampleAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
