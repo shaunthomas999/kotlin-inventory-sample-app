86ed7557ea265d7cb0c2206e1deb9ebcb8bbfe08
Shaun Thomas <shaunthomas999@gmail.com>
2017-08-18 09:01:39 +0200

    Removed auto-generation of ID for Product domain

--- src/main/kotlin/com/shaunthomas999/inventory/Product.kt
+++ src/main/kotlin/com/shaunthomas999/inventory/Product.kt
@@ -2,17 +2,13 @@ package com.shaunthomas999.inventory
 
 import java.util.*
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 data class Product(
-        @Id @GeneratedValue(strategy = GenerationType.AUTO)
-        var id: Int,
+        @Id var id: Int,
         var name: String,
         var manufacturer: String,
         var manufacturingDate: Date,
-        var expiryDate: Date,
         var stock: Int,
         var safetyStock: Int)
